60adfc7875c40c6a9be0276d5fcbd811bed82698
Abraham Krisnanda <abraham.krisnanda@gmail.com>
2015-12-05 16:45:22 +0800

    Add versionist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: 'test@example.com', name:'test_user', password: '1234', password_confirmation: '1234')
