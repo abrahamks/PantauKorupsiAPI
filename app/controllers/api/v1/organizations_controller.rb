25c994c46faac56534c270852ed1be8ee81acb7a
Abraham Krisnanda <abraham.krisnanda@gmail.com>
2015-12-06 16:20:25 +0800

    Add organizations#show

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::OrganizationsController < Api::V1::BaseController
   before_action :authenticate_with_token!, only: :create
+  before_action :set_organization, only: [:show, :update]
   respond_to :json
   def index
     # localhost:3000/v1/organizations?q[name_cont]=Z
     q = Organization.ransack(params[:q])
     organizations = q.result(distinct: true).page(params[:page]).per(params[:per_page])
     paginate json: organizations
+  end
+
+  def show
+    render json: @organization
   end
 
   def create
@@ -18,6 +23,9 @@ class Api::V1::OrganizationsController < Api::V1::BaseController
   end
 
   private
+    def set_organization
+      @organization = Organization.find(params[:id])
+    end
     def organization_params
       params.permit(:name, :organization_url, :description)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   api_version(:module => "Api::V1", :path => {:value => "v1"}, :default => true, :defaults => {:format => :json}) do
-    resources :organizations, only: [:index, :create]
+    resources :organizations, only: [:index, :create, :show]
     resources :actors, only: [:index, :create ,:show]
     resources :sessions, only: [:create, :destroy]
     resources :users, only: [:show, :create]
     resources :issues, only: [:index, :show, :create] do
-      # resources :actors, controller: 'issuess_actors'
+      resources :involvements
       resources :feeds do
         get 'verify', to: :verify
       end
